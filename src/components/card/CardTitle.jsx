a291528e045980d862946474dffd3e9774d90015
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-25 17:42:06 +0300

    the end

--- src/components/card/CardTitle.jsx
+++ src/components/card/CardTitle.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
 import "./Card.css"
-const CardTitle = ({name,img,options}) => {
+const CardTitle = ({ name, img, options }) => {
     const [toggle, setToggle] = useState(true)
-    const hendalMouseEnter =(e)=>{
-        e.target.querySelector('img').style.transform="scale(0.8)"
+    const handleMouseEnter = (e) => {
+        e.target.querySelector('img').style.transform = 'scale(0.8)'
+        e.target.style.transform ='rotate(5deg)'
     }
-    const hendalMouseLeave =(e)=>{
-        e.target.querySelector('img').style.transform="scale(1)"
+    const handleMouseLeave = (e) => {
+        e.target.querySelector('img').style.transform = 'scale(1)'
+        e.target.style.transform ='rotate(0deg)'
     }
-  return (
-    <div className='card col-6 ' role="button" onClick={()=>setToggle(!toggle)} onMouseEnter={hendalMouseEnter} onMouseLeave={hendalMouseLeave}>
-        {toggle ? <div>
-        <img src={img} width="120px" height="100px" alt=''/>
-        <h6>{name}</h6>
-        </div>:  <div className='cardback'>
-               {options.map((item,e)=>(
-                <ul key={e}>
-                    <li>{item}</li>
-                </ul>
-               ))}
-        </div>}
-       
-       
-    </div>
-  )
+    
+    return (
+        <div className='card col-6' role='button' onClick={() => setToggle(!toggle)} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+            {toggle ?
+                <div>
+                    <img src={img} alt="" width='150px' height='100px' />
+                    <h6>{name}</h6>
+                </div>
+                :
+                <div className='cardback'>
+                    {options.map((item, e) => (
+                        <ul key={e}>
+                            <li>{item}</li>
+                        </ul>
+                    ))}
+                </div>
+            }
+
+
+        </div>
+
+    )
 }
 
 export default CardTitle
