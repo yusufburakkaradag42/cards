a8dbe30f1e0707b22a9a9e869ff938596176b0d5
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-25 16:46:56 +0300

    cardback

--- src/components/card/CardTitle.jsx
+++ src/components/card/CardTitle.jsx
@@ -3,10 +3,17 @@ import "./Card.css"
 const CardTitle = ({name,img,options}) => {
   return (
     <div className='card col-6 '>
-     
-        <img src={img} width="100px" height="100px" alt=''/>
+        <div>
+        <img src={img} width="120px" height="100px" alt=''/>
         <h6>{name}</h6>
-      
+        </div>
+        <div className='cardback'>
+               {options.map((item,e)=>(
+                <ul key={e}>
+                    <li>{item}</li>
+                </ul>
+               ))}
+        </div>
     </div>
   )
 }
