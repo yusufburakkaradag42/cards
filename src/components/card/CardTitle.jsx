cf01142d263f22e2a8685df8d70b6b871f76d724
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-25 16:59:39 +0300

    finish

--- src/components/card/CardTitle.jsx
+++ src/components/card/CardTitle.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Card.css"
 const CardTitle = ({name,img,options}) => {
+    const [toggle, setToggle] = useState(true)
   return (
-    <div className='card col-6 '>
-        <div>
+    <div className='card col-6 ' role="button" onClick={()=>setToggle(!toggle)}>
+        {toggle ? <div>
         <img src={img} width="120px" height="100px" alt=''/>
         <h6>{name}</h6>
-        </div>
-        <div className='cardback'>
+        </div>:  <div className='cardback'>
                {options.map((item,e)=>(
                 <ul key={e}>
                     <li>{item}</li>
                 </ul>
                ))}
-        </div>
+        </div>}
+       
+       
     </div>
   )
 }
